2170d734634228356993b2f8ca266e87f8dec836
Stephan Fowler <stephanfowler@gmail.com>
2012-08-06 22:53:18 +0100

    Added isReload boolean argument to afterLoad callback fn.

--- jquery.responsive-content.js
+++ jquery.responsive-content.js
@@ -25,7 +25,7 @@ $.fn.responsiveContent = function( useropts ){
 		useropts 
 	);
 
-	var target = this, nowLoading; 
+	var target = this, isReload = true, nowLoading; 
 
 	// Return Ajax request params, pefixed _rescon. Basically screen width, and the device capabilities passed in.
 	getResconParams = (function(){
@@ -92,7 +92,8 @@ $.fn.responsiveContent = function( useropts ){
 	// Fire post load actions (on a zero timeout, for good luck)
 	function doAfterLoad(){
 		setTimeout( function(){
-			opts.afterLoad();
+			opts.afterLoad(isReload);
+			isReload = false;
 		});
 	};
 
